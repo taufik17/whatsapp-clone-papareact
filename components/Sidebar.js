27614f4e051569bb5eae0557011da0d3b72280a0
taufik17 <taufikagungsantoso17@gmail.com>
2022-08-23 19:09:27 +0700

    setup awal

---
+++ components/Sidebar.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+function Sidebar() {
+  return <Container>
+    <h1>Sidebar</h1>
+  </Container>;
+}
+
+export default Sidebar;
+
+const Container = styled.div``;
